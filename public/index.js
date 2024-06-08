9afea8b3e533491b65c9afeb946f4689cab2d92f
Mmesoma-Constance <mmesomanwokem@gmail.com>
2024-06-08 01:31:50 +0100

    fifth commit

--- public/index.js
+++ public/index.js
@@ -27,6 +27,24 @@ let coinPoints = 5;
 
 let stakedCoin = false;
 
+// CONSECUTIVE WIN AUDIO
+
+let consecutiveWinsAudio = document.getElementById("consecutiveWinsAudio");
+
+// STAKE WIN AUDIO
+
+let stakedAudio = document.getElementById("stakedAudio");
+
+// STAKE LOSS AUDIO
+
+let errorAudio = document.getElementById("errorAudio");
+
+consecutiveWinsAudio.pause();
+stakedAudio.pause();
+errorAudio.pause();
+
+// CONFETTI FUNCTIONALITY
+
 document.querySelectorAll(".confettiButton").forEach((eBtn) => {
   eBtn.addEventListener("click", function () {
     // Trigger confetti explosion
@@ -34,8 +52,13 @@ document.querySelectorAll(".confettiButton").forEach((eBtn) => {
   });
 });
 
+document.getElementById("highStake").classList.remove("beat-text");
+
+let bronzeCoin = localStorage.getItem("bronzeCoin") || 0;
+document.getElementById("bronzeCoin").textContent = bronzeCoin;
+
 document.getElementById("bronzeCoinBtn").disabled = true;
-document.getElementById("bronzeCoinBtn").style.opacity = "80%";
+document.getElementById("bronzeCoinBtn").style.opacity = "70%";
 
 let highStakeText = 0;
 document.querySelector(".highStakeText").textContent = highStakeText;
@@ -43,12 +66,13 @@ highStake.disabled = true;
 highStake.style.opacity = "60%";
 
 function stakingUpdate() {
-  if (coinPoints >= 10 && !stakedCoin && !stakeClick) {
+  if (coinPoints > 10 && !stakedCoin && !stakeClick) {
     highStakeText = 1;
     document.querySelector(".highStakeText").textContent = highStakeText;
+    document.getElementById("highStake").classList.add("beat-text");
   }
 
-  if (coinPoints < 10 || highStakeText === 0) {
+  if (coinPoints <= 10 || highStakeText === 0) {
     highStake.disabled = true;
     highStake.style.opacity = "60%";
   } else {
@@ -60,18 +84,22 @@ function stakingUpdate() {
 
 stakingUpdate();
 
-function winning() {
+function winning(increment) {
+  coinPoints += increment;
+
+  //LOSING THE GAME
   if (coinPoints < 1) {
     coinPoints = 0;
     points.textContent = coinPoints;
     document.getElementById("gameOver").style.display = "block";
-    document.getElementById("coin").textContent = coinFace;
 
     document.getElementById("gameOver").textContent = "YOU LOST, GAME OVER!";
     losses++;
     lossText.textContent = losses;
 
-    stakingUpdate();
+    errorAudio.pause();
+
+    // stakingUpdate();
 
     document.body.style.backgroundColor = "#FF0000";
     let overAudio = document.getElementById("overAudio");
@@ -79,11 +107,12 @@ function winning() {
 
     playAgainBtn.textContent = "Start Over";
 
-    document.getElementById("coin").style.opacity = "80%";
-
     document.getElementById("coin").disabled = true;
+    document.getElementById("coin").style.opacity = "70%";
     document.getElementById("head").disabled = true;
     document.getElementById("tail").disabled = true;
+    document.getElementById("bronzeCoinBtn").disabled = true;
+    document.getElementById("bronzeCoinBtn").style.opacity = "70%";
 
     document.querySelectorAll(".btn").forEach((e) => {
       e.style.opacity = "60%";
@@ -97,6 +126,7 @@ function winning() {
     endGame.style.opacity = "60%";
   }
 
+  // WINING THE GAME
   if (coinPoints >= 40) {
     document.getElementById("gameOver").style.display = "block";
     document.getElementById("coin").textContent = coinFace;
@@ -104,28 +134,22 @@ function winning() {
     document.getElementById("gameOver").textContent = "YOU WON THE GAME!";
     document.querySelector(".bronzeWin").textContent =
       " YOU HAVE GOT 1 BRONZE COIN";
-    wins++;
-    winText.textContent = wins;
 
     confetti();
 
-    consecutiveWinsAudio.pause();
-    stakedAudio.pause();
     document.body.style.backgroundColor = "#60b347";
+
     let victoryAudio = document.getElementById("victoryAudio");
     victoryAudio.play();
 
     playAgainBtn.textContent = "Start Over";
 
-    bronzeCoin++;
-    document.getElementById("bronzeCoin").textContent = bronzeCoin;
-
-    localStorage.setItem("bronzeCoin", JSON.stringify(bronzeCoin));
-
     document.getElementById("coin").disabled = true;
-    document.getElementById("coin").style.opacity = "80%";
+    document.getElementById("coin").style.opacity = "70%";
     document.getElementById("head").disabled = true;
     document.getElementById("tail").disabled = true;
+    document.getElementById("bronzeCoinBtn").disabled = true;
+    document.getElementById("bronzeCoinBtn").style.opacity = "70%";
 
     document.querySelectorAll(".btn").forEach((e) => {
       e.style.opacity = "60%";
@@ -139,33 +163,88 @@ function winning() {
     endGame.style.opacity = "60%";
     stakingUpdate();
   }
+
+  //  winning();
 }
 
-winning();
+// HIGHSTAKE BUTTON FUNCTIONALITY
+document.getElementById("highStake").addEventListener("click", function () {
+  stakedCoin = false;
+  stakeClick = true;
+  // winning(-10);
+  // points.textContent = coinPoints;
+  // console.log(coinPoints);
+
+  if (highStakeText === 1) {
+    highStake.disabled = true;
+    highStake.style.opacity = "60%";
+    document.getElementById("highStake").classList.remove("beat-text");
+  }
+  // stakeFn();
+  if (coinPoints > 10 && !stakedCoin && highStakeText >= 1) {
+    document.getElementById("snackbar").style.display = "flex";
+    setTimeout(function () {
+      document.getElementById("snackbar").style.display = "none";
+    }, 1000);
+
+    let stake = 10;
+    winning(-stake);
+    points.textContent = coinPoints;
+
+    highStakeText = 0;
+    document.querySelector(".highStakeText").textContent = highStakeText;
+    document.getElementById("highStake").classList.remove("beat-text");
+  }
+
+  stakedCoin = false;
+});
+
+function bronzeIncreement() {
+  if (coinPoints >= 40) {
+    wins++;
+    winText.textContent = wins;
+
+    bronzeCoin++;
+    document.getElementById("bronzeCoin").textContent = bronzeCoin;
+
+    localStorage.setItem("bronzeCoin", bronzeCoin);
+    document.getElementById("coin").textContent = coinFace;
+  }
+  if (bronzeCoin >= 1) {
+    document.querySelector(".bronzeCoinColor").classList.add("flipping");
+  }
+}
+
+bronzeIncreement();
+
+// winning();
 
 document.getElementById("snackbarText").textContent = "10 coins staked!";
-
-let bronzeCoin = JSON.parse(localStorage.getItem("bronzeCoin")) || 0;
-document.getElementById("bronzeCoin").textContent = bronzeCoin;
 
 document
   .getElementById("bronzeCoinText")
   .addEventListener("click", function () {
     if (bronzeCoin >= 1) {
-      coinPoints += 10;
+      winning(10);
       points.textContent = coinPoints;
+      introText.textContent = "+10";
+      introText.style.opacity = "100%";
+      introText.classList.add("animate-up");
+
+      setTimeout(function () {
+        introText.style.opacity = "0%";
+        introText.classList.remove("animate-up");
+      }, 1000);
       bronzeCoin--;
       document.getElementById("bronzeCoin").textContent = bronzeCoin;
 
-      localStorage.setItem("bronzeCoin", JSON.stringify(bronzeCoin));
+      localStorage.setItem("bronzeCoin", bronzeCoin);
 
-      //  // console.log("bronze clicked");
-
+      bronzeIncreement();
       stakingUpdate();
-
-      winning();
-    } else {
-      // console.log("bronze not clicked");
+    }
+    if (bronzeCoin < 1) {
+      document.querySelector(".bronzeCoinColor").classList.remove("flipping");
     }
   });
 
@@ -178,7 +257,7 @@ let playAgainBtn = document.getElementById("playAgain");
 const endGame = document.getElementById("endGame");
 
 document.getElementById("coin").disabled = true;
-document.getElementById("coin").style.opacity = "80%";
+document.getElementById("coin").style.opacity = "70%";
 
 document.getElementById("head").disabled = true;
 
@@ -202,6 +281,7 @@ playAgainBtn.addEventListener("click", function () {
 
   highStake.disabled = true;
   highStake.style.opacity = "60%";
+  document.getElementById("highStake").classList.remove("beat-text");
 
   document.getElementById("bronzeCoinBtn").disabled = false;
   document.getElementById("bronzeCoinBtn").style.opacity = "100%";
@@ -238,10 +318,96 @@ playAgainBtn.addEventListener("click", function () {
   document.getElementById("gameOver").style.display = "none";
 });
 
+const introText = document.getElementById("introText");
+
 // HEAD/TAIL BUTTON FUNCTIONALITY
 document.querySelectorAll(".btn").forEach((button) => {
   button.addEventListener("click", function () {
-    winning();
+    buttonValue = button.textContent;
+    document.getElementById("coin").textContent = coinFace;
+
+    document.getElementById("highStake").classList.remove("beat-text");
+
+    // STAKING 10 COINS
+    if (
+      buttonValue === coinFace &&
+      !stakedCoin &&
+      stakeClick &&
+      coinPoints < 40
+    ) {
+      winning(25 - 2);
+      points.textContent = coinPoints;
+
+      introText.textContent = "+25";
+      introText.style.opacity = "100%";
+      introText.classList.add("animate-up");
+
+      setTimeout(function () {
+        introText.style.opacity = "0%";
+        introText.classList.remove("animate-up");
+      }, 1000);
+
+      stakedCoin = true;
+      document.getElementById("highStake").classList.remove("beat-text");
+
+      //  winning();
+      document.getElementById("snackbar").style.display = "flex";
+      document.getElementById("snackbarText").textContent =
+        "You have earned 25 coins!";
+      setTimeout(function () {
+        document.getElementById("snackbar").style.display = "none";
+      }, 1200);
+
+      stakedAudio.play();
+    } else if (buttonValue !== coinFace && !stakedCoin && stakeClick) {
+      winning(2);
+      points.textContent = coinPoints;
+
+      document.getElementById("highStake").classList.remove("beat-text");
+
+      stakedCoin = true;
+      document.getElementById("snackbar").style.display = "flex";
+      document.getElementById("snackbarText").textContent =
+        "You lost the stake";
+      setTimeout(function () {
+        document.getElementById("snackbar").style.display = "none";
+      }, 1100);
+
+      errorAudio.play();
+    }
+
+    // ADDING COIN POINTS
+    if (buttonValue === coinFace && coinPoints >= 1) {
+      winning(2);
+      consecutiveWins++;
+      points.textContent = coinPoints;
+
+      button.style.backgroundColor = "green";
+      button.style.color = "white";
+      setTimeout(function () {
+        button.style.backgroundColor = "";
+        button.style.color = "black";
+      }, 700);
+
+      document.querySelector(".consecutiveWinsText").textContent =
+        consecutiveWins;
+    } else if (buttonValue !== coinFace && coinPoints >= 1) {
+      // DEDUCT COIN POINTS
+      winning(-2);
+      points.textContent = coinPoints;
+
+      button.style.backgroundColor = "red";
+      button.style.color = "white";
+      setTimeout(function () {
+        button.style.backgroundColor = "";
+        button.style.color = "black";
+      }, 700);
+
+      consecutiveWins = 0;
+      document.querySelector(".consecutiveWinsText").textContent =
+        consecutiveWins;
+    }
+
     // stakedCoin = false;
 
     document.getElementById("head").disabled = true;
@@ -253,163 +419,40 @@ document.querySelectorAll(".btn").forEach((button) => {
 
     document.getElementById("coin").textContent = coinFace;
 
-    if (coinPoints < 40 && coinPoints === 0) {
-      setTimeout(function () {
-        document.getElementById("coin").textContent = "?";
-      }, 800);
-    }
-
-    buttonValue = button.textContent;
-    if (buttonValue === coinFace && coinPoints >= 1) {
-      coinPoints += 2;
-      consecutiveWins++;
-      points.textContent = coinPoints;
-
-      winning();
-      document.querySelector(".consecutiveWinsText").textContent =
-        consecutiveWins;
-    } else if (coinPoints >= 1) {
-      coinPoints -= 2;
-      points.textContent = coinPoints;
-      consecutiveWins = 0;
-      document.querySelector(".consecutiveWinsText").textContent =
-        consecutiveWins;
-    }
-    winning();
-
+    //CONSECUTIVE WINS
     if (consecutiveWins === 3) {
-      coinPoints += 7 - 2;
+      winning(5);
       points.textContent = coinPoints;
+      introText.textContent = "+5";
+      introText.style.opacity = "100%";
+      introText.classList.add("animate-up");
 
-      winning();
+      setTimeout(function () {
+        introText.style.opacity = "0%";
+        introText.classList.remove("animate-up");
+      }, 1000);
+
       consecutiveWins = 0;
       document.querySelector(".consecutiveWinsText").textContent =
         consecutiveWins;
 
-      let consecutiveWinsAudio = document.getElementById(
-        "consecutiveWinsAudio"
-      );
       consecutiveWinsAudio.play();
     }
 
+    if (coinPoints < 40 || coinPoints === 0) {
+      document.getElementById("coin").textContent = coinFace;
+      setTimeout(function () {
+        document.getElementById("coin").textContent = "?";
+      }, 600);
+    }
+
+    // highStakeText = 0;
+    // document.querySelector(".highStakeText").textContent = highStakeText;
+
+    bronzeIncreement();
+
     stakingUpdate();
-
-    // if (coinPoints >= 10 && !stakedCoin && !stakeClick) {
-    //   highStakeText = 1;
-    //   document.querySelector(".highStakeText").textContent = highStakeText;
-    // }
-
-    // if (coinPoints >= 40) {
-    //   document.getElementById("gameOver").style.display = "block";
-    //   document.getElementById("coin").textContent = coinFace;
-
-    //   document.getElementById("gameOver").textContent = "YOU WON THE GAME!";
-    //   document.querySelector(".bronzeWin").textContent =
-    //     " YOU HAVE GOT 1 BRONZE COIN";
-    //   wins++;
-    //   winText.textContent = wins;
-
-    //   document.body.style.backgroundColor = "#60b347";
-    //   let victoryAudio = document.getElementById("victoryAudio");
-    //   victoryAudio.play();
-
-    //   playAgainBtn.textContent = "Start Over";
-
-    //   bronzeCoin++;
-    //   document.getElementById("bronzeCoin").textContent = bronzeCoin;
-
-    //   localStorage.setItem("bronzeCoin", JSON.stringify(bronzeCoin));
-
-    //   document.getElementById("coin").disabled = true;
-    //   document.getElementById("coin").style.opacity = "80%";
-    //   document.getElementById("head").disabled = true;
-    //   document.getElementById("tail").disabled = true;
-
-    //   document.querySelectorAll(".btn").forEach((e) => {
-    //     e.style.opacity = "60%";
-    //   });
-
-    //   gamePlay = false;
-    //   playAgainBtn.disabled = false;
-    //   playAgainBtn.style.backgroundColor = "white";
-    //   playAgainBtn.style.opacity = "100%";
-    //   endGame.disabled = true;
-    //   endGame.style.opacity = "60%";
-    // }
   });
-});
-
-// HIGHSTAKE BUTTON FUNCTIONALITY
-document.getElementById("highStake").addEventListener("click", function () {
-  stakedCoin = false;
-  stakeClick = true;
-  winning();
-
-  if (highStakeText === 1) {
-    highStake.disabled = true;
-    highStake.style.opacity = "60%";
-  }
-  // stakeFn();
-  if (coinPoints >= 10 && !stakedCoin && highStakeText >= 1) {
-    document.getElementById("snackbar").style.display = "flex";
-    setTimeout(function () {
-      document.getElementById("snackbar").style.display = "none";
-    }, 1000);
-
-    let stake = 10;
-    coinPoints -= stake;
-    points.textContent = coinPoints;
-    // console.log("staked");
-
-    highStakeText = 0;
-    document.querySelector(".highStakeText").textContent = highStakeText;
-  }
-
-  document.querySelectorAll(".btn").forEach((button) => {
-    button.addEventListener("click", function () {
-      if (buttonValue === coinFace && !stakedCoin && stakeClick) {
-        coinPoints += 25 - 2;
-        points.textContent = coinPoints;
-
-        stakedCoin = true;
-
-        winning();
-        document.getElementById("snackbar").style.display = "flex";
-        document.getElementById("snackbarText").textContent =
-          "You have earned 25 coins!";
-        setTimeout(function () {
-          document.getElementById("snackbar").style.display = "none";
-        }, 1100);
-
-        let stakedAudio = document.getElementById("stakedAudio");
-        stakedAudio.play();
-      } else if (
-        buttonValue !== coinFace &&
-        !stakedCoin &&
-        coinPoints >= 1 &&
-        stakeClick
-      ) {
-        coinPoints += 2;
-        points.textContent = coinPoints;
-
-        stakedCoin = true;
-        document.getElementById("snackbar").style.display = "flex";
-        document.getElementById("snackbarText").textContent =
-          "You lost the stake";
-        setTimeout(function () {
-          document.getElementById("snackbar").style.display = "none";
-        }, 1100);
-
-        let errorAudio = document.getElementById("errorAudio");
-        errorAudio.play();
-      }
-
-      // highStakeText = 0;
-      // document.querySelector(".highStakeText").textContent = highStakeText;
-    });
-  });
-
-  stakedCoin = false;
 });
 
 // COIN FLIP CLICK
@@ -429,6 +472,7 @@ document.getElementById("coin").addEventListener("click", function () {
     this.style.animation = "flip .2s ease-in-out infinite";
 
     document.getElementById("coin").textContent = "?";
+    introText.style.opacity = "0%";
     coinFace = Math.random();
 
     if (coinFace < 0.5) {
@@ -436,7 +480,8 @@ document.getElementById("coin").addEventListener("click", function () {
     } else {
       coinFace = "Tail";
     }
-    console.log(coinFace);
+    // console.log(coinFace);
+
     setTimeout(function () {
       document.getElementById("coin").style.animation = "none";
     }, 500); // Stop animation after 10 seconds
@@ -446,6 +491,10 @@ document.getElementById("coin").addEventListener("click", function () {
 // END GAME BUTTON FUNCTIONALITY
 function resetGame() {
   endGame.addEventListener("click", function () {
+    // if (document.getElementById("highStake").classList.add("beat-text")) {
+    //   document.getElementById("highStake").classList.remove("beat-text");
+    // }
+    document.getElementById("highStake").classList.remove("beat-text");
     gamePlay = false;
     stakedCoin = false;
     stakeClick = false;
@@ -454,7 +503,7 @@ function resetGame() {
     document.getElementById("tail").disabled = true;
     document.getElementById("endGame").disabled = true;
     endGame.style.opacity = "60%";
-    document.getElementById("coin").style.opacity = "80%";
+    document.getElementById("coin").style.opacity = "70%";
 
     document.querySelectorAll(".btn").forEach((e) => {
       e.style.opacity = "60%";
@@ -464,9 +513,18 @@ function resetGame() {
     playAgainBtn.style.opacity = "100%";
 
     document.getElementById("bronzeCoinBtn").disabled = true;
-    document.getElementById("bronzeCoinBtn").style.opacity = "80%";
+    document.getElementById("bronzeCoinBtn").style.opacity = "70%";
 
-    stakingUpdate();
+    // stakingUpdate();
+
+    if (coinPoints <= 10 || highStakeText === 0) {
+      highStake.disabled = true;
+      highStake.style.opacity = "60%";
+    } else {
+      highStake.disabled = false;
+
+      highStake.style.opacity = "100%";
+    }
 
     document.getElementById("snackbar").style.display = "none";
     document.getElementById("snackbarText").textContent = "10 coins staked!";
@@ -479,7 +537,6 @@ function resetGame() {
     playAgainBtn.textContent = "Start Over";
 
     coinFace = null;
-    // console.log(coinFace);
 
     coinPoints = 5;
     points.textContent = coinPoints;
